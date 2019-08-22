1f814c1e0b58059e587ef13b34dfcd67ede6aaac
Samitha Padukkage Don <samitha.312@gmail.com>
2019-08-22 10:32:26 +1000

    Updates

---
+++ precache-manifest.2c89241312a650caa9af4a75bf42fb55.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "322c66cf1573024d637ffa0341ebbdf6",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "049801eed2beda0e4285",
+    "url": "/pomodoro-timer/static/css/2.64795fbd.chunk.css"
+  },
+  {
+    "revision": "911d1967afe6c4214465",
+    "url": "/pomodoro-timer/static/css/main.c1cd5534.chunk.css"
+  },
+  {
+    "revision": "049801eed2beda0e4285",
+    "url": "/pomodoro-timer/static/js/2.6fbcbd78.chunk.js"
+  },
+  {
+    "revision": "911d1967afe6c4214465",
+    "url": "/pomodoro-timer/static/js/main.2ec19f86.chunk.js"
+  },
+  {
+    "revision": "8d84c672a99e0f0ece83",
+    "url": "/pomodoro-timer/static/js/runtime~main.2a0df6ba.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pomodoro-timer/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pomodoro-timer/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pomodoro-timer/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pomodoro-timer/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pomodoro-timer/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
